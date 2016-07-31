ef3bec61832242f648612d6958eabe0c044c0add
u174762 <sam.panga@hotmail.com>
2016-07-31 17:27:22 +0530

    Add files via upload

---
+++ ACD_Javab_session1.Assignment4/src/Assignment4.java
@@ -0,0 +1,20 @@
+
+public class Assignment4 {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			int x=5;int y =10;
+			int sum,div,mul,sub;
+			sum=x + y * 2;
+	        div=x - y + 2;
+	        mul= (x+y) * 2;
+	        sub= y % x;
+	System.out.println(sum);
+	System.out.println(div);
+	System.out.println(mul);
+	System.out.println(sub);
+
+		}
+
+	}
